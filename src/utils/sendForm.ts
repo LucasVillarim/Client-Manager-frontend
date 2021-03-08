81158a58484f5513c80ad1b0ab924d48d71aac55
LucasVillarim <villarim94@gmail.com>
2021-03-08 01:04:48 -0300

    put hook outside function

--- src/utils/sendForm.ts
+++ src/utils/sendForm.ts
@@ -3,6 +3,8 @@ import { useGlobal } from 'Context';
 import { FormProps, UserProps } from 'interfaces';
 import api from 'services/api';
 
+const { setMainList, setModal } = useGlobal();
+
 export function clearEmptyRows(tableData: any) {
   const cleanTable = tableData.filter(
     (element: any) => element.product !== '' && element.price !== '',
@@ -16,7 +18,6 @@ export async function SendForm(
   total: number | string,
   tableData?: any,
 ): Promise<Response> {
-  const { setMainList, setModal } = useGlobal();
   const table = clearEmptyRows(tableData);
   let response: any;
   try {
